ea08052e00a666972296eb31cfd92272682db7e5
marius93rm <marius93rm@gmail.com>
2025-10-28 14:05:28 +0200

    Remove unused frontend scripts

--- js/script.js
+++ js/script.js
@@ -1,116 +1,39 @@
-(function($) {
+(() => {
+  "use strict";
 
-    "use strict";
-                // init Chocolat light box
-                var initChocolat = function () {
-                  Chocolat(document.querySelectorAll('.image-link'), {
-                    imageSize: 'contain',
-                    loop: true,
-                  })
-                }
+  const updateHeaderOnScroll = () => {
+    const header = document.getElementById("primary-header");
+    if (!header) {
+      return;
+    }
 
-        document.addEventListener("DOMContentLoaded", function(){
-        
-          window.addEventListener('scroll', function() {
-              
-            if (window.scrollY > 50) {
-              document.getElementById('primary-header').classList.add('fixed-top');
-            } else {
-              document.getElementById('primary-header').classList.remove('fixed-top');
-              // remove padding top from body
-              document.body.style.paddingTop = '0';
-            } 
-          });
-        }); 
-        // DOMContentLoaded  end
+    if (window.scrollY > 50) {
+      header.classList.add("fixed-top");
+    } else {
+      header.classList.remove("fixed-top");
+      document.body.style.paddingTop = "0";
+    }
+  };
 
+  const initTestimonialSlider = () => {
+    const testimonialSlider = document.querySelector(".testimonial-swiper");
+    if (!testimonialSlider || typeof Swiper === "undefined") {
+      return;
+    }
 
+    new Swiper(testimonialSlider, {
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  };
 
-        $(document).ready(function() {
+  document.addEventListener("DOMContentLoaded", () => {
+    updateHeaderOnScroll();
+    window.addEventListener("scroll", updateHeaderOnScroll, { passive: true });
+    initTestimonialSlider();
+  });
+})();
 
-          initChocolat();
-  
-
-          $(".user-items .search-item").click(function(){
-              $(".search-box").toggleClass('active');
-              $(".search-box .search-input").focus();
-            });
-            $(".close-button").click(function(){
-              $(".search-box").toggleClass('active');
-            });  
-
-          var swiper = new Swiper(".testimonial-swiper", {
-            loop: true,
-            pagination: {
-              el: ".swiper-pagination",
-              clickable: true,
-            },
-          }); 
-
-
-          var swiper = new Swiper(".team-swiper", {
-            slidesPerView: 2,
-            spaceBetween: 20,
-            pagination: {
-              el: "#our-team .swiper-pagination",
-              clickable: true,
-            },
-            breakpoints: {
-              0: {
-                slidesPerView: 1,
-                spaceBetween: 20,
-              },
-              1200: {
-                slidesPerView: 2,
-                spaceBetween: 10,
-              },
-            },
-          });  
-
-          window.addEventListener("load", (event) => {
-            //isotope
-            $('.isotope-container').isotope({
-              // options
-              itemSelector: '.item',
-              layoutMode: 'masonry'
-            });
-      
-      
-      
-            // Initialize Isotope
-            var $container = $('.isotope-container').isotope({
-              // options
-              itemSelector: '.item',
-              layoutMode: 'masonry'
-            });
-      
-            $(document).ready(function () {
-              //active button
-              $('.filter-button').click(function () {
-                $('.filter-button').removeClass('active');
-                $(this).addClass('active');
-              });
-            });
-      
-            // Filter items on button click
-            $('.filter-button').click(function () {
-              var filterValue = $(this).attr('data-filter');
-              if (filterValue === '*') {
-                // Show all items
-                $container.isotope({ filter: '*' });
-              } else {
-                // Show filtered items
-                $container.isotope({ filter: filterValue });
-              }
-            });
-      
-          });
-      
-      
-
-
-        }); // End of a document      
-
-
-
-    })(jQuery);
